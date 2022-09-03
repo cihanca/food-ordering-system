c1d38db3ef83d5a3647326cc982a55e6038351c4
bellcarslan <Cihan.Arslan@vodafone.com>
2022-09-03 16:24:17 +0300

    payment domain core implementation completed

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/org/food/ordering/system/payment/service/domain/event/PaymentCompletedEvent.java
@@ -0,0 +1,14 @@
+package org.food.ordering.system.payment.service.domain.event;
+
+import org.food.ordering.system.payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.Collections;
+
+public class PaymentCompletedEvent extends PaymentEvent{
+
+    public PaymentCompletedEvent(Payment payment, ZonedDateTime createdAt) {
+        super(payment, createdAt, Collections.emptyList());
+    }
+
+}
