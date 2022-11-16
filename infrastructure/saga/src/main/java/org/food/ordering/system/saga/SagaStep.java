c86a49d35d2c71663252154aa4aa5794b32cd819
bellcarslan <Cihan.Arslan@vodafone.com>
2022-11-16 10:37:35 +0300

    saga implementation

---
+++ infrastructure/saga/src/main/java/org/food/ordering/system/saga/SagaStep.java
@@ -0,0 +1,11 @@
+package org.food.ordering.system.saga;
+
+import org.food.ordering.system.domain.event.DomainEvent;
+
+public interface SagaStep<T, S extends DomainEvent, U extends DomainEvent> {
+
+    S process(T data);
+
+    U rollback(T data);
+
+}
